cb1b2716845d364e2ce463202b363a42b99b1ec7
Yuriy Nikolaev <warlokk83@gmail.com>
2021-02-08 23:09:13 +0400

    Methods divided

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,55 @@
+package ru.netology.domain;
+
+public class Radio {
+    private String name;
+    private int minStation = 0;
+    private int maxStation = 9;
+    private int currentStation;
+    private int minVolume = 0;
+    private int maxVolume = 10;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int newStation) {
+        this.currentStation = newStation;
+    }
+
+    public void setCurrentStationUp() {
+        if (getCurrentStation() == maxStation) {
+            setCurrentStation(minStation);
+        } else
+            setCurrentStation(getCurrentStation()+1);
+    }
+
+    public void setCurrentStationDown() {
+        if (getCurrentStation() == minStation) {
+            setCurrentStation(maxStation);
+        } else
+            setCurrentStation(getCurrentStation()-1);
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+    public void setCurrentVolume(int newVolume) {
+        this.currentVolume = newVolume;
+    }
+
+    public void setCurrentVolumeUp() {
+        if (getCurrentVolume() == maxVolume)
+            return;
+        else
+            setCurrentVolume(getCurrentVolume()+1);
+    }
+
+    public void setCurrentVolumeDown() {
+        if (getCurrentVolume() == minVolume)
+            return;
+        else
+            setCurrentVolume(getCurrentVolume()-1);
+    }
+
+}
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,4 +1,4 @@
-package ru.netology;
+package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,23 +14,25 @@ class RadioTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/volumeData.csv", delimiter = '|', numLinesToSkip = 1)
     void shouldChangeVolume(String test, int initialVolume, String changeVolume, int expected) {
-        settings.setCurrentVolumeTest(initialVolume);
-        settings.setCurrentVolume(changeVolume);
+        settings.setCurrentVolume(initialVolume);
+        if (changeVolume.equals("+")) settings.setCurrentVolumeUp();
+        if (changeVolume.equals("-")) settings.setCurrentVolumeDown();
         assertEquals(expected, settings.getCurrentVolume());
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/stationData.csv", delimiter = '|', numLinesToSkip = 1)
     void shouldChangeStation(String test, int initialStation, String changeStation, int expected) {
-        settings.setCurrentStationTest(initialStation);
-        settings.setCurrentStation(changeStation);
+        settings.setCurrentStation(initialStation);
+        if (changeStation.equals("next")) settings.setCurrentStationUp();
+        if (changeStation.equals("prev")) settings.setCurrentStationDown();
         assertEquals(expected, settings.getCurrentStation());
     }
 
     @ParameterizedTest
     @CsvSource({"5", "0", "9"})
     void shouldChangeStationRC(int inputRC) {
-        settings.setCurrentStationRC(inputRC);
+        settings.setCurrentStation(inputRC);
         assertEquals(inputRC, settings.getCurrentStation());
     }
 }
